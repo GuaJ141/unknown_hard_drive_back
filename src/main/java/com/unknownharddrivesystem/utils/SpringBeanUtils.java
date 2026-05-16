c72ddfe0cb4052eb059d35ece76687c1f5130717
GuaJ141 <260803133@qq.com>
2026-05-16 21:10:56 +0800

    Create SpringBeanUtils.java

---
+++ src/main/java/com/unknownharddrivesystem/utils/SpringBeanUtils.java
@@ -0,0 +1,21 @@
+package com.unknownharddrivesystem.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) {
+        applicationContext = context;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+}
