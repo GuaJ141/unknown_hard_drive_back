7b61af9c04ec26c51b59d2dc4bb8cb7c33a6d449
GuaJ141 <260803133@qq.com>
2026-05-16 21:11:01 +0800

    Create GetHttpSessionConfigurator.java

---
+++ src/main/java/com/unknownharddrivesystem/ws/GetHttpSessionConfigurator.java
@@ -0,0 +1,19 @@
+package com.unknownharddrivesystem.ws;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.HandshakeResponse;
+import jakarta.websocket.server.HandshakeRequest;
+import jakarta.websocket.server.ServerEndpointConfig;
+import org.springframework.context.annotation.Configuration;
+
+
+public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator {
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession httpSession = (HttpSession)request.getHttpSession();
+        if (httpSession != null) {
+            sec.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        }
+
+    }
+}
