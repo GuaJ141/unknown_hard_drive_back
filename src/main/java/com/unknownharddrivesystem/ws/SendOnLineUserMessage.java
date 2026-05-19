27debd3d25a41e158aeabde439144efe394f46c3
GuaJ141 <260803133@qq.com>
2026-05-20 00:31:31 +0800

    加入optionCode，更改userUId为object

--- src/main/java/com/unknownharddrivesystem/ws/SendOnLineUserMessage.java
+++ src/main/java/com/unknownharddrivesystem/ws/SendOnLineUserMessage.java
@@ -1,34 +1,46 @@
 package com.unknownharddrivesystem.ws;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class SendOnLineUserMessage {
     private Boolean systemInfo;
 
-    private Set<Integer> userUId;
+    //0用户下线 1用户上线 2上线首次获取在线用户
+    private int optionCode;
+
+    private Object userUId;
 
     public SendOnLineUserMessage() {
     }
 
-    public SendOnLineUserMessage(boolean systemInfo, Set<Integer> userUId) {
+    public SendOnLineUserMessage(Boolean systemInfo, int optionCode, Object userUId) {
         this.systemInfo = systemInfo;
+        this.optionCode = optionCode;
         this.userUId = userUId;
-
     }
 
-    public boolean isSystemInfo() {
+    public Boolean getSystemInfo() {
         return systemInfo;
     }
 
-    public void setSystemInfo(boolean systemInfo) {
+    public void setSystemInfo(Boolean systemInfo) {
         this.systemInfo = systemInfo;
     }
 
-    public Set<Integer> getUserUId() {
+    public int getOptionCode() {
+        return optionCode;
+    }
+
+    public void setOptionCode(int optionCode) {
+        this.optionCode = optionCode;
+    }
+
+    public Object getUserUId() {
         return userUId;
     }
 
-    public void setUserUId(Set<Integer> userUId) {
+    public void setUserUId(Object userUId) {
         this.userUId = userUId;
     }
 }
