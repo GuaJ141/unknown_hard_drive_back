46a23f1fd8c70c0b93214b8904db7e647eea84e6
GuaJ141 <260803133@qq.com>
2026-05-22 00:50:24 +0800

    redis配置类更新

--- src/main/java/com/unknownharddrivesystem/config/RedisConfig.java
+++ src/main/java/com/unknownharddrivesystem/config/RedisConfig.java
@@ -1,4 +1,32 @@
 package com.unknownharddrivesystem.config;
 
+import com.unknownharddrivesystem.utils.FastJson2RedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
 public class RedisConfig{
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        FastJson2RedisSerializer<Object> serializer = new FastJson2RedisSerializer<>(Object.class);
+
+        // key 采用 String 序列化方式
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // hash 的 key 也采用 String 序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
 }
--- src/main/java/com/unknownharddrivesystem/utils/SpringBeanUtil.java
+++ src/main/java/com/unknownharddrivesystem/utils/SpringBeanUtil.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanUtils implements ApplicationContextAware {
+public class SpringBeanUtil implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
