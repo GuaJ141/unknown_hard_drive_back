1450f59c4ea6400031933be5f4eb065126c4092e
GuaJ141 <260803133@qq.com>
2026-05-22 00:53:14 +0800

    新的测试

--- src/test/java/com/unknownharddrivesystem/UnknownharddrivesystemApplicationTests.java
+++ src/test/java/com/unknownharddrivesystem/UnknownharddrivesystemApplicationTests.java
@@ -1,37 +1,34 @@
 package com.unknownharddrivesystem;
 
-import com.unknownharddrivesystem.entity.FriendAddMessage;
-import com.unknownharddrivesystem.entity.FriendList;
+import com.unknownharddrivesystem.UnknownharddrivesystemApplication;
 import com.unknownharddrivesystem.entity.HdUser;
-import com.unknownharddrivesystem.mapper.FriendMapper;
 import com.unknownharddrivesystem.mapper.HdUserMapper;
-import jakarta.websocket.Endpoint;
-import jakarta.websocket.EndpointConfig;
-import jakarta.websocket.Session;
+import com.unknownharddrivesystem.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tools.jackson.databind.ObjectMapper;
 
-import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UnknownharddrivesystemApplicationTests {
 
 	@Autowired
-	HdUserMapper hdUserMapper;
-
+	RedisUtil redisUtil;
 	@Autowired
-	FriendMapper friendMapper;
+	HdUserMapper userMapper;
+
 	@Test
 	void contextLoads() {
-		FriendAddMessage friendAddMessage = new FriendAddMessage();
-		friendAddMessage.setMessage("你还好吗");
-		friendAddMessage.setSourceUserId(55);
-		friendAddMessage.setToUserId(57);
+		String key = "69";
+		HdUser user = userMapper.selectUserById(69);
+		redisUtil.set(key, user, 30, TimeUnit.SECONDS);
 
-		int res = friendMapper.sendMessage(friendAddMessage);
-		System.out.println(res);
+		ObjectMapper Jackson = new ObjectMapper();
+		//HdUser temp = Jackson.readValue(redisUtil.get(key).toString(), HdUser.class);
+		System.out.println(redisUtil.get(key).toString());
+
 	}
-
 }
