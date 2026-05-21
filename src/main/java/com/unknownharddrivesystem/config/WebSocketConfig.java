d47aa3c94aa18455106eaf7e3e69a82363395441
GuaJ141 <260803133@qq.com>
2026-05-22 00:50:30 +0800

    Update WebSocketConfig.java

--- src/main/java/com/unknownharddrivesystem/config/WebSocketConfig.java
+++ src/main/java/com/unknownharddrivesystem/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.unknownharddrivesystem.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
